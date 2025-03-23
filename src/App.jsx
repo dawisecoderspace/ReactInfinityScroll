1e4678ff2f4dce4a6b4d00cdb95f628d21a28337
dawisecoderspace <dawiseofficechannel@gmail.com>
2025-03-23 20:38:44 +0100

    Updated InfinityScroll component with customizable props and added comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import InfinityScroll from './components/InfinityScroll';
+import InfinityScroll from './components/InfinityScroll'
 import './App.css'
 
 
 
+// Sample items array for the infinity scroll
 const items = [
   { name: 'ReactJS', img: '/react.svg', article: 'Lorem ipsum dolor sit amet consectetur adipisicint.' },
   { name: 'Vite', img: '/vite.svg', article: 'Lorem ipsum dolor sit amet consectetur adipisicing.' },
   { name: 'JavaScript', img: '/javascript.svg', article: 'Lorem ipsum dolor sit amet consectetur adipisicing.' }
-];
+]
 
 
 
@@ -17,6 +18,7 @@ function App() {
     <div className="App">
       <h1>React Infinity Scroll</h1>
       <p>Scroll infinitely through the items below</p>
+      {/* InfinityScroll component with some default props */}
       <InfinityScroll items={items} width={360} />
     </div>
   )
@@ -25,3 +27,4 @@ function App() {
 
 
 export default App
+
--- src/components/InfinityScroll.jsx
+++ src/components/InfinityScroll.jsx
@@ -4,65 +4,130 @@ import PropTypes from 'prop-types'
 import './InfinityScroll.css'
 
 
-function InfinityScroll({ items, width = 360 }) {
+
+function InfinityScroll({ 
+  items, 
+  width = 360, 
+  scrollerHeight = 400, 
+  cardBgColor = '#888888', 
+  cardNameColor = '#444444', 
+  cardImgSize = '50%', 
+  cardTextColor = '#ffffff', 
+  articleFontSize = '13px' 
+}) {
+  // Duplicating the items for infinite scrolling
   const [cards] = useState([...items, ...items])
   const scrollerRef = useRef(null)
 
 
+  // Scroll the content to the initial position when component mounts
   useEffect(() => {
     if (scrollerRef.current) {
       scrollerRef.current.scrollLeft = width;
     }
   }, [width])
 
+
+  // Function to scroll left
   const goToLeft = () => {
     if (scrollerRef.current) {
-      scrollerRef.current.scrollBy({ left: width, behavior: 'smooth' });
+      scrollerRef.current.scrollBy({ left: width, behavior: 'smooth' })
     }
 
-    
+    // Reset scroll position if we've scrolled enough
     if (scrollerRef.current.scrollLeft >= scrollerRef.current.scrollWidth / 2) {
       setTimeout(() => {
         scrollerRef.current.scrollLeft = width;
-      }, width * 2);
+      }, width * 2)
     }
-  }
+  };
 
+
+  // Function to scroll right
   const goToRight = () => {
     if (scrollerRef.current) {
-      scrollerRef.current.scrollBy({ left: -width, behavior: 'smooth' });
+      scrollerRef.current.scrollBy({ left: -width, behavior: 'smooth' })
     }
 
-    
+    // Reset scroll position if we're at the beginning
     if (scrollerRef.current.scrollLeft === 0) {
       setTimeout(() => {
-        scrollerRef.current.scrollLeft = scrollerRef.current.scrollWidth / 2;
-        scrollerRef.current.scrollBy({ left: -width, behavior: 'smooth' });
-      }, 5); 
+        scrollerRef.current.scrollLeft = scrollerRef.current.scrollWidth / 2
+        scrollerRef.current.scrollBy({ left: -width, behavior: 'smooth' })
+      }, 5)
     }
   }
+
 
   return (
     <div className="content-scroller">
+      {/* Left arrow button */}
       <GrPrevious className="button" onClick={goToLeft} />
-      <div className="scroller" style={{ width }} ref={scrollerRef}>
+
+      {/* Scrollable content container */}
+      <div 
+        className="scroller" 
+        style={{ 
+          width,
+          height: `${scrollerHeight}px` 
+        }} 
+        ref={scrollerRef}
+      >
         {cards.map((card, index) => (
-          <div key={index} className="card" style={{ width }}>
-            <h1>{card.name}</h1>
-            <img src={card.img} alt="cover" />
-            <p>{card.article}</p>
+          <div 
+            key={index} 
+            className="card" 
+            style={{ 
+              width, 
+              backgroundColor: cardBgColor 
+            }}
+          >
+            {/* Card name */}
+            <h1 
+              style={{ color: cardNameColor }}
+            >
+              {card.name}
+            </h1>
+            
+            {/* Card image */}
+            <img 
+              src={card.img} 
+              alt="cover" 
+              style={{ width: cardImgSize }} 
+            />
+
+            {/* Card article */}
+            <p 
+              style={{ 
+                color: cardTextColor, 
+                fontSize: articleFontSize 
+              }}
+            >
+              {card.article}
+            </p>
           </div>
         ))}
       </div>
+
+      {/* Right arrow button */}
       <GrNext className="button" onClick={goToRight} />
     </div>
   )
 }
 
 
+
 InfinityScroll.propTypes = {
   items: PropTypes.array.isRequired,
   width: PropTypes.number,
+  scrollerHeight: PropTypes.number,
+  cardBgColor: PropTypes.string,
+  cardNameColor: PropTypes.string,
+  cardImgSize: PropTypes.string,
+  cardTextColor: PropTypes.string,
+  articleFontSize: PropTypes.string
 }
 
-export default InfinityScroll;
+
+
+export default InfinityScroll
